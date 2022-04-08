ba0900eaa0d72d97219a85d7a113cf814aea02a4
naomysilva <naomyleticia92@gmail.com>
2022-04-08 14:54:42 -0300

    tela de exibicao modificada

--- gui.py
+++ gui.py
@@ -1,47 +1,47 @@
+
 from tkinter import *
-def dados():
-    if Nome.get()=="" or Cpf.get() == "" or Matricula.get() == "" or Data.get() == "":
-        x = Label (interface , text="Dados Não Enviados" , background="Red",width=20).place(x=200 , y=200)
 
-    else:
-        x = Label(interface,text="Dados Enviados",background="green",width=20).place(x=200,y=200)
-        with open("nomes.txt","a",encoding="utf-8") as file:
-            file.write(f"Nome do album: {Nome.get()} nome do artista: {Cpf.get()} Data de lancamento do album: {Matricula.get()} ultimo album lançado: {Data.get()}\n")
-        Nome.delete(0,END)
-        Cpf.delete(0,END)
-        Matricula.delete(0,END)
-        Data.delete(0,END)
-def destruir():
-    interface.destroy()
 
-interface = Tk()
-interface.title("Dados Usuario")
-interface.geometry("500x600")
-interface.configure(background='Black')
-Label(interface,text="Nome do album:",background="white").pack(anchor=W)
-Nome=Entry(interface)
-Nome.pack(anchor=W)
-Label(interface,text="Nome do artista:",background="white").pack(anchor=W)
-Cpf=Entry(interface)
-Cpf.pack(anchor=W)
+janela = Tk()
 
-Label(interface,text="Data de lancamento do album:",background="white").pack(anchor=W)
-Matricula=Entry(interface)
-Matricula.pack(anchor=W)
-Label(interface,text="foi o ultimo album lançado?",background="white").pack(anchor=W)
-Data=Entry(interface)
-Data.pack(anchor=W)
-botao = Button(interface,text="Enviar Dados", command=dados)
-botao.place(x=0,y=160)
-botao1 = Button(interface,text="Mostrar Dados", command=destruir)
-botao1.place (x=0,y=200)
-interface.mainloop()
-interface1 = Tk()
-interface1.title("Dados Sistema")
-interface1.geometry("1000x600")
-interface1.configure(background='Black')
-with open("nomes.txt","r",encoding="utf-8") as file:
-    for x in file.readlines():
-        Label (interface1 , text=f"{x}" , background="white").pack (anchor=W)
+def principal():    
+    tela()
+    janela.mainloop()
+    
+def exibicao():
+    global janela
+    janela.destroy()
+    janela = Tk()
+    janela.title("                                                                          Menu Principal")
+    janela.config(background="#49A")
+    janela.geometry("700x550")
+    janela.maxsize(width=900, height= 700)
+    img = PhotoImage(file="artist arsenal.png")
+    label_imagem = Label(janela,image=img).pack()
+    botoes()
+    janela.mainloop()
 
-interface1.mainloop()
+    
+def tela():
+    global janela
+    janela.destroy()
+    janela = Tk()
+    janela.title("                                                                          Menu Principal")
+    janela.config(background="#49A")
+    janela.geometry("700x550")
+    janela.maxsize(width=900, height= 700)
+    janela.mainloop()
+
+
+def botoes():
+  botaoCadastro = Button(janela,text="Cadastrar Artista", activebackground="blue",bg="lightblue",width=50)
+  botaoCadastro.place(x=170,y=330)
+  botaoListar = Button(janela,text="Listagem", activebackground="blue",bg="lightblue",width=50)
+  botaoListar.place(x=170,y=370)
+  botaoBuscar = Button(janela,text="Procurar", activebackground="blue",bg="lightblue",width=50)
+  botaoBuscar.place(x=170,y=410)
+  botaoSair = Button(janela,text="Sair", activebackground="blue",bg="lightblue",width=50)
+  botaoSair.place(x=170,y=450)
+
+exibicao()
+    
