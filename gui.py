677ef86fb0b0602bffc55b3ab8ba2f0376302063
naomysilva <naomyleticia92@gmail.com>
2022-04-09 00:11:22 -0300

    mudanca de cor de backgraud

--- gui.py
+++ gui.py
@@ -13,7 +13,7 @@ def exibicao():
     janela.destroy()
     janela = Tk()
     janela.title("                                                                          Menu Principal")
-    janela.config(background="v ")
+    janela.config(background="lightblue")
     janela.geometry("700x550")
     janela.maxsize(width=900, height= 700)
     img = PhotoImage(file="artist arsenal.png")
@@ -32,15 +32,18 @@ def tela():
     janela.maxsize(width=900, height= 700)
     janela.mainloop()
 
+
+
 #botoes e label
 def botoes():
+  global botaoCadastro , botaoListar
   botaoCadastro = Button(janela,text="Cadastrar Artista", activebackground="blue",bg="lightblue",width=50)
   botaoCadastro.place(x=170,y=330)
   botaoListar = Button(janela,text="Listagem", activebackground="blue",bg="lightblue",width=50)
   botaoListar.place(x=170,y=370)
-  botaoBuscar = Button(janela,text="Procurar", activebackground="blue",bg="lightblue",width=50)
+  botaoBuscar =Button(janela,text="Buscar", activebackground="blue",bg="lightblue",width=50)
   botaoBuscar.place(x=170,y=410)
-  botaoSair = Button(janela,text="Sair", activebackground="blue",bg="lightblue",width=50)
+  botaoSair = Button(janela,text="Sair", activebackground="blue",bg="lightblue",width=50)  
   botaoSair.place(x=170,y=450)
 
 exibicao()
